4d5f83e76b28064eec5c97ed2ab2d6f5767d3656
Marat <igralkin@gmail.com>
2021-10-15 13:37:48 +0300

    Cleaning, generalizing and commenting

--- xml2csv.php
+++ xml2csv.php
@@ -1,12 +1,17 @@
 <?php
-$xmldata='https://www.connectccs.gr/xml/all-products.xml';
-$csvFile = 'connectccs.csv';
+$xmldata='SOURCE_URL.xml';
+$csvFile = 'results.csv';
+$xPath = '//product';
+
+// One of this array must be empty, or INCLUDE columns, or EXCLUDE
+$included_columns = array();  $excluded_columns = array('description');
+
+// Use function - generates CSV
+xml2csv($xmldata, $csvFile, $xPath, $included_columns, $excluded_columns);
 
 
-echo '<pre>';
-// ----------
-// xml2csv
-// -----
+
+
 function xml2csv($xmlFile, $csvFile, $xPath, $included_columns = array(), $excluded_columns = array()) {
     $columns = array(); // All possible columns in XML
     $all_data = array();
@@ -25,21 +30,11 @@ function xml2csv($xmlFile, $csvFile, $xPath, $included_columns = array(), $exclu
             if (!in_array($key, $columns)) {
                 array_push($columns, $key);
             }
-            $item_data[$key] = $value;
-            //print "$key<br>";
-			$csvData .= '"' . trim($value) . '"' . ',';
-		
+            $item_data[$key] = $value;	
 		}
-        
-		// Trim off the extra comma
-		$csvData = trim($csvData, ',');
-		
-		// Add an LF
-		$csvData .= "\n";
         
         // Add item data for all data
         array_push($all_data, $item_data);
-	
 	}
     
     
@@ -52,12 +47,14 @@ function xml2csv($xmlFile, $csvFile, $xPath, $included_columns = array(), $exclu
         }
     }
     
-    
     // Delete excluded columns from columns
-    if (!empty($excluded_columns)) {
+    if (!empty($included_columns)) {
+        $columns = $included_columns;
+    }    
+    else if (!empty($excluded_columns)) {
         $columns = array_diff($columns, $excluded_columns);
     }
-    print_r($columns);
+
     // Cleaned and ordered data
     $ordered_all_data = array();
     foreach($all_data as $item_data) {
@@ -68,14 +65,13 @@ function xml2csv($xmlFile, $csvFile, $xPath, $included_columns = array(), $exclu
         array_push($ordered_all_data, $ordered_item);
     }
     
-    //print_r($ordered_all_data);
     
     // Open CSV file to write
     
     $fp = fopen($csvFile, 'w');
     // Write header to CSV
     $fields = implode(',', $columns);
-    //fputcsv($fp, $fields);
+
     fwrite($fp,$fields.PHP_EOL);
 
     // Write data to CSV
@@ -88,11 +84,6 @@ function xml2csv($xmlFile, $csvFile, $xPath, $included_columns = array(), $exclu
 
     fclose($fp);
     
-    
-	// Return the CSV data
-	return $csvData;
-	
+	return True;
 }
-$data = xml2csv($xmldata, $csvFile, '//product', '', array('description'));
-//echo '<pre>';
-//print($data);
+?>
