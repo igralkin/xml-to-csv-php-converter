c849b35cc2ef2e0d2b2b08a15d68c0f2b1ddd518
Marat <igralkin@gmail.com>
2021-10-27 22:47:30 +0300

    Minor corrections
    Minor corrections

--- xml2csv.php
+++ xml2csv.php
@@ -10,43 +10,45 @@ $included_columns = array();  $excluded_columns = array('description');
 // Use function - generates CSV
 xml2csv($xmldata, $csvFile, $xPath, $included_columns, $excluded_columns, $delimiter);
 
-
 function xml2csv($xmlFile, $csvFile, $xPath, $included_columns = array(), $excluded_columns = array(), $delimiter = ',') {
     $columns = array(); // All possible columns in XML
     $all_data = array();
-	// Load the XML file
-	if (function_exists('simplexml_load_file')) {
-	    $xml = simplexml_load_file($xmlFile);
-	} else {
-	    print "<h3>simplexml_load_file function does not exists! Please, install php-xml</h3>";
-	    return False;
-	}
+    // Load the XML file
+    if (function_exists('simplexml_load_file')) {
+        $xml = simplexml_load_file($xmlFile);
+    } else {
+        print "<h3>simplexml_load_file function does not exists! Please, install php-xml</h3>";
+        return False;
+    }
 
-	// Jump to the specified xpath
-	$path = $xml->xpath($xPath);
+    // Jump to the specified xpath
+    $path = $xml->xpath($xPath);
 
-	 // Loop through attributes
+    // Loop through the specified xpath
+    foreach($path as $item) {
+        
+        $item_data = array();
+        
+        // Loop through attributes
         foreach($item[0]->attributes() as $key => $value) {
             if (!in_array($key, $columns)) {
                 array_push($columns, $key);
             }
-            $item_data[$key] = (string)$value;	
+            $item_data[$key] = (string)$value;    
         }
-	// Loop through the specified xpath
-	foreach($path as $item) {
         
-        	$item_data = array();
-		// Loop through the elements in this xpath
-		foreach($item as $key => $value) {
+        // Loop through the elements in this xpath
+        foreach($item as $key => $value) {
+            //print "$key $value<br>";
             if (!in_array($key, $columns)) {
                 array_push($columns, $key);
             }
-            $item_data[$key] = $value;	
-		}
+            $item_data[$key] = $value;    
+        }
         
         // Add item data for all data
         array_push($all_data, $item_data);
-	}
+    }
     
     
     // Fill all columns for empty values
@@ -57,7 +59,6 @@ function xml2csv($xmlFile, $csvFile, $xPath, $included_columns = array(), $exclu
             }
         }
     }
-    
     // Delete excluded columns from columns
     if (!empty($included_columns)) {
         $columns = $included_columns;
@@ -66,6 +67,7 @@ function xml2csv($xmlFile, $csvFile, $xPath, $included_columns = array(), $exclu
         $columns = array_diff($columns, $excluded_columns);
     }
 
+
     // Cleaned and ordered data
     $ordered_all_data = array();
     foreach($all_data as $item_data) {
@@ -73,6 +75,7 @@ function xml2csv($xmlFile, $csvFile, $xPath, $included_columns = array(), $exclu
         foreach($columns as $column) {
             $ordered_item[$column] = $item_data[$column];
         }
+
         array_push($ordered_all_data, $ordered_item);
     }
     
@@ -87,6 +90,10 @@ function xml2csv($xmlFile, $csvFile, $xPath, $included_columns = array(), $exclu
 
     // Write data to CSV
     foreach ($ordered_all_data as $item_data) {
+        $item_values = implode(',', array_values($item_data));
+        //print_r($item_data);
+        //fputcsv($fp, $item_values);
+        //fwrite($fp,$item_values.PHP_EOL);
         fputcsv($fp, $item_data, $delimiter);
     }
 
